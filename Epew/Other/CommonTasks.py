c8f8929821f00de25d2c41769bae2f7601306543
Marius Göcke <marius.goecke@gmail.com>
2022-12-08 11:32:16 +0100

    fixed building

--- Epew/Other/CommonTasks.py
+++ Epew/Other/CommonTasks.py
@@ -13,8 +13,8 @@ def common_tasks():
     version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../Epew/Epew.csproj", folder_of_current_file), version)
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../EpewTests/EpewTests.csproj", folder_of_current_file), version)
-    tfcps = TasksForCommonProjectStructure()
-    tfcps.standardized_tasks_do_common_tasks(file, 1, "QualityCheck", sys.argv)
+    t = TasksForCommonProjectStructure()
+    t.standardized_tasks_do_common_tasks(file, version, 1, "QualityCheck", True, None, sys.argv)
 
 
 if __name__ == "__main__":
