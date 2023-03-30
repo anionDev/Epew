66fd60631defdb83baecf308e910b35899a701c4
Marius Göcke <marius.goecke@gmail.com>
2023-03-30 10:22:30 +0200

    Revert "refactoring for main-method"
    This reverts commit 99119b5de32f9b861d996083e1abce2f86db246b.

--- Epew/Epew/Program.cs
+++ Epew/Epew/Program.cs
@@ -1,15 +1,8 @@
-﻿using GRYLibrary.Core.Miscellaneous;
-
-namespace Epew.Epew.Core
+﻿namespace Epew.Epew.Core
 {
     internal static class Program
     {
-        internal static int Main(string[] commandlineArguments)
-        {
-            var gryConsoleApplication = new GRYConsoleApplication<EpewOptions>(MainI);
-            return gryConsoleApplication.Main(commandlineArguments);
-        }
-        internal static int MainI(string[] arguments)
+        internal static int Main(string[] arguments)
         {
             return new ProgramExecutor().Main(arguments);
         }
