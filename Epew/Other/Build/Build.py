1ae650517ae44aef84313cc026dda448e4ffc1e5
Marius Göcke <marius.goecke@gmail.com>
2024-05-21 17:58:35 +0200

    updated constantsnames

--- Epew/Other/Build/Build.py
+++ Epew/Other/Build/Build.py
@@ -11,11 +11,7 @@ def build():
     t = TasksForCommonProjectStructure()
     codeunit_folder = GeneralUtilities.resolve_relative_path("../../..", this_file)
     t.standardized_tasks_build_for_dotnet_project(this_file, "QualityCheck", t.get_default_target_environmenttype_mapping(), ["win-x64", "linux-x64"], verbosity, sys.argv)
-    t.create_deb_package_for_artifact(codeunit_folder,
-                                      t.get_constant_value(codeunit_folder, "maintainername"),
-                                      t.get_constant_value(codeunit_folder, "maintaineremail"),
-                                      t.get_constant_value(codeunit_folder, "description"),
-                                      verbosity, sys.argv)
+    t.create_deb_package_for_artifact(codeunit_folder, t.get_constant_value(codeunit_folder, "MaintainerName"), t.get_constant_value(codeunit_folder, "MaintainerEMailAddress"), t.get_constant_value(codeunit_folder, "CodeUnitDescription") verbosity, sys.argv)
 
 
 if __name__ == "__main__":
