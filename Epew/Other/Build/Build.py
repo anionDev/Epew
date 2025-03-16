819e7204a996a377af3e7169f2f4c66789838310
Marius Göcke <marius.goecke@gmail.com>
2025-03-16 17:01:22 +0100

    added creating of zip package

--- Epew/Other/Build/Build.py
+++ Epew/Other/Build/Build.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 from ScriptCollection.GeneralUtilities import GeneralUtilities
-from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
 
 
@@ -12,6 +11,7 @@ def build():
     codeunit_folder = GeneralUtilities.resolve_relative_path("../../..", this_file)
     t.standardized_tasks_build_for_dotnet_project(this_file, "QualityCheck", t.get_default_target_environmenttype_mapping(), ["win-x64", "linux-x64"], verbosity, sys.argv)
     t.create_deb_package_for_artifact(codeunit_folder, t.get_constant_value(codeunit_folder, "MaintainerName"), t.get_constant_value(codeunit_folder, "MaintainerEMailAddress"), t.get_constant_value(codeunit_folder, "CodeUnitDescription"), verbosity, sys.argv)
+    t.create_zip_file_for_artifact(codeunit_folder, "BuildResult_DotNet_win-x64", "Epew-Zip-for-Windows", verbosity, sys.argv)
 
 
 if __name__ == "__main__":
